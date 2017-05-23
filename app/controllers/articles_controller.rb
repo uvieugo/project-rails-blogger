7d9449e5bce3eb35b2ba1cb5c17289f0c875d3e5
Ugo Uvie <uvie.ugo@gmail.com>
2017-05-23 04:43:33 +0100

    Tagging feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,10 +39,11 @@ class ArticlesController < ApplicationController
         redirect_to article_path(@article) 
     end
         
-    
+
+
     private
     def article_params
-        params.require(:article).permit(:title, :body)
+        params.require(:article).permit(:title, :body, :tag_list)
     end
     
     
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,25 @@
 class Article < ApplicationRecord
     has_many :comments
+    has_many :taggings
+    has_many :tags, through: :taggings
+
+    # def tag_list
+    #     self.tags.collect do |tag|
+    #         tag.name
+    #     end.join(", ")  
+    # end
+
+    def tag_list
+        tags.join(", ")
+    end
+
+    def tag_list=(tags_string)
+        # split tag string
+        tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+        # loop thru tag_names, find or create the new tags
+        new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+        #attach found tags to article
+        self.tags = new_or_found_tags
+
+    end
 end
